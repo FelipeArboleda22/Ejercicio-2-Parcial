d6a2e00bf79554b7dfa85a109c9f63a51d8af0f3
eFelipeArboleda22 <felipekaro22@gmil.com>
2024-03-22 19:50:24 -0500

    Ejercicio realizado

---
+++ promedio.py
@@ -0,0 +1,27 @@
+print("Debes ingresar 10 números")
+print("Ingrese el número 1:")
+num1 = float(input())
+print("Ingrese el número 2:")
+num2 = float(input())
+print("Ingrese el número 3:")
+num3 = float(input())
+print("Ingrese el número 4:")
+num4 = float(input())
+print("Ingrese el número 5:")
+num5 = float(input())
+print("Ingrese el número 6:")
+num6 = float(input())
+print("Ingrese el número 7:")
+num7 = float(input())
+print("Ingrese el número 8:")
+num8 = float(input())
+print("Ingrese el número 9:")
+num9 = float(input())
+print("Ingrese el número 10:")
+num10 = float(input())
+
+
+suma_total = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10
+promedio = suma_total / 10
+
+print("El promedio de los números ingresados es:", promedio)
