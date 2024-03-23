32b7a86b6764ed86fbc08bbfc7d47e4c9eb3d70b
eFelipeArboleda22 <felipekaro22@gmil.com>
2024-03-22 19:58:56 -0500

    Ejercicio corregido con y almacenados el una lista

--- promedio.py
+++ promedio.py
@@ -1,27 +1,14 @@
-print("Debes ingresar 10 números")
-print("Ingrese el número 1:")
-num1 = float(input())
-print("Ingrese el número 2:")
-num2 = float(input())
-print("Ingrese el número 3:")
-num3 = float(input())
-print("Ingrese el número 4:")
-num4 = float(input())
-print("Ingrese el número 5:")
-num5 = float(input())
-print("Ingrese el número 6:")
-num6 = float(input())
-print("Ingrese el número 7:")
-num7 = float(input())
-print("Ingrese el número 8:")
-num8 = float(input())
-print("Ingrese el número 9:")
-num9 = float(input())
-print("Ingrese el número 10:")
-num10 = float(input())
+
+print("Ingrese 10 números:")
+
+numeros = []
+for numerosalm in range(10):
+    ingresonumero = float(input(f"Ingrese el número {numerosalm+1}: "))
+    numeros.append(ingresonumero)
 
 
-suma_total = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10
-promedio = suma_total / 10
+total = sum(numeros)
+promedio = total / len(numeros)
 
-print("El promedio de los números ingresados es:", promedio)
+
+print(f"El promedio de los números ingresados es: {promedio}")
